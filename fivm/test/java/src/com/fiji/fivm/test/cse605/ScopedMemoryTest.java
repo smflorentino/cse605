356fe082a5df2427a55a4a82eaf9291c757be387
Scott Florentino <scottflo@email.fake>
2015-04-14 10:29:55 -0400

    add sample project

--- fivm/test/java/src/com/fiji/fivm/test/cse605/ScopedMemoryTest.java
+++ fivm/test/java/src/com/fiji/fivm/test/cse605/ScopedMemoryTest.java
@@ -1,7 +1,34 @@
 package com.fiji.fivm.test.cse605;
 
+import javax.realtime.LTMemory;
+
 /**
  * Created by scottflo on 4/14/15.
  */
 public class ScopedMemoryTest {
+    private LTMemory ltMemory;
+
+    public static void main(String[] args)
+    {
+        LTMemory scoped1 = new LTMemory(8*1024 + 64);
+        scoped1.enter(new Fib());
+    }
+    public static class Fib implements Runnable {
+
+        long[] numbers;
+        @Override
+        public void run() {
+            numbers = new long[1024];
+            numbers[0] = 1;
+            numbers[1] = 1;
+            for(int i =2;i<numbers.length;i++)
+            {
+                numbers[i] = numbers[i-1] + numbers[i-2];
+            }
+            for(int i = 0;i<numbers.length; i++)
+            {
+                System.out.println(numbers[i]);
+            }
+        }
+    }
 }
--- tools/rtsjsample/src/MemExample.java
+++ tools/rtsjsample/src/MemExample.java
@@ -2,4 +2,5 @@
  * Created by scottflo on 4/14/15.
  */
 public class MemExample {
+
 }
